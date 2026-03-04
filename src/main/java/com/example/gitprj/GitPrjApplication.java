118fdd6785196f12ecc9312c0191d34094f41554
leemiran-dev <leemiran.0729@gmail.com>
2026-03-04 16:36:11 +0900

    init commit

---
+++ src/main/java/com/example/gitprj/GitPrjApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPrjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitprj/GitPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
