25a8a31cf3f014261ae51d8a814f8ddda73feb31
leemiran-dev <leemiran.0729@gmail.com>
2026-03-05 12:48:21 +0900

    test2 브랜치에 테스트 파일 생성

---
+++ src/main/java/com/example/gitprj/TestMerge.java
@@ -0,0 +1,7 @@
+package com.example.gitprj;
+
+public class TestMerge {
+    public static void main(String[] args) {
+        System.out.println("test2 브랜치에 파일 생성 후 main 브랜치로 머지 테스트 하는 중입니다.");
+    }
+}
